81dea354024e5e50edcb57521b03288d70998f56
Nico Guba <nguba@mac.com>
2019-03-06 17:12:05 +0000

    Basic project structure for layered implementation

---
+++ presentation/src/main/java/election/bully/ElectionBullyApplication.java
@@ -0,0 +1,13 @@
+package election.bully;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionBullyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionBullyApplication.class, args);
+	}
+
+}
---
+++ presentation/src/test/java/election/bully/ElectionBullyApplicationTests.java
@@ -0,0 +1,16 @@
+package election.bully;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElectionBullyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
